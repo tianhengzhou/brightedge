1153e043a95a128e9bd53406bcebecc4b7e49f22
tianhengzhou <zth198814@gmail.com>
2016-11-23 16:29:13 -0800

    Initial Commit

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -1,3 +1,29 @@
 /**
  * Created by tianheng on 11/23/16.
  */
+// Set Refrence To Utilized DOM Elements
+var circles = document.getElementById('selCircles');
+var loadButton = document.getElementById('btnLoadGrid');
+var dataTable = document.getElementById('divDataTable');
+function jsDataTable() {
+  this.domTable = document.createElement('table');
+  this.domTableBody = document.createElement('tbody');
+  this.domTableHead = document.createElement('thead');
+  this.TableHeadArray = [];
+  this.TableRows = [];
+  this.addHeadItem = function (strItem) {
+    this.TableHeadArray.push(strItem)
+  };
+  this.addRow = function (arrayRow) {
+    this.TableRows.push(arrayRow)
+  };
+  this.setTableId = function (strId) {
+    this.domTable.id = strId;
+  };
+  this.setTableName = function (strClassName) {
+    this.domTable.className = strClassName;
+  };
+  this.createDataTable = function (strApplyTo) {
+    
+  }
+}
